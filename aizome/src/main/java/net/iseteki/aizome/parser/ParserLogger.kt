c260273985e14016555989135d94e728b1a04bc9
Nobuhiro Ito <iseebi@iseteki.net>
2025-04-13 23:06:56 +0900

    separate definition

--- aizome/src/main/java/net/iseteki/aizome/parser/ParserLogger.kt
+++ aizome/src/main/java/net/iseteki/aizome/parser/ParserLogger.kt
@@ -2,14 +2,6 @@ package net.iseteki.aizome.parser
 
 import net.iseteki.aizome.logger.AizomeLogger
 
-sealed class ParserWarning {
-    data class UnclosedTag(val index: Int) : ParserWarning()
-    data class UnopenedMarkup(val tag: String, val index: Int) : ParserWarning()
-    data class UnclosedMarkup(val tag: String, val index: Int) : ParserWarning()
-    data class EmptyTag(val index: Int) : ParserWarning()
-    data class UnknownFormat(val format: String) : ParserWarning()
-}
-
 interface ParserLogger {
     fun warning(warning: ParserWarning)
 }
